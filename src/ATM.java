abd399dcf6b32e83cf4b5e93e5bf121716029a68
Zubaba Ahmed <zubabaahmed@Zubabas-MacBook-Air-2.local>
2022-11-08 00:24:29 -0600

    first

---
+++ src/ATM.java
@@ -0,0 +1,11 @@
+public class ATM {
+    //create: create an account
+    //read: read your transaction/balance
+    //update: update/deposit money
+    //delete: delete/cancel transaction; withdraw money
+    //1: Check balance \n" + "-----------> read
+    //2: Deposit\n" + " -----------------> update
+    //3: Previous transaction \n" + " ---> read
+    //4: Calculate transaction \n" + " --> create
+    //5: Exit \n"; ---------------------->
+}
---
+++ src/Account.java
@@ -0,0 +1,13 @@
+public class Account {
+    private String name;
+    private int accountNumber;
+    private int balance;
+    private boolean deposit;
+
+    public Account(String name, int accountNumber, int balance, boolean deposit) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.deposit = deposit;
+    }
+}
